f8d7ad75f918052be6d6fa22388d14d599c9df4b
Nicolas Granados <ngranados@abstrahere.com>
2022-06-10 13:20:18 -0400

    First Commit

---
+++ pages/timeline/index.js
@@ -0,0 +1,13 @@
+export default function TimeLine() {
+  return (
+    <>
+      <h1>Esto es Timeline</h1>
+      <style jsx>{`
+        h1 {
+          font-size: 36px;
+          color: red;
+        }
+      `}</style>
+    </>
+  );
+}
