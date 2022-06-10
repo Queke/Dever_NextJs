03d881ab00b7b33b1cb88fc9b642edbfadde6c2c
Nicolas Granados <ngranados@abstrahere.com>
2022-06-10 18:38:32 -0400

    se agrega Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -14,7 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Hola</h1>
         <nav>
-          <a href="/timeline">TimeLine</a>
+          <Link href="/timeline">TimeLine</Link>
         </nav>
       </main>
     </div>
--- pages/timeline/index.js
+++ pages/timeline/index.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function TimeLine() {
   return (
     <>
       <h1>Esto es Timeline</h1>
+      <Link href="/">Go Home</Link>
       <style jsx>{`
         h1 {
           font-size: 36px;
